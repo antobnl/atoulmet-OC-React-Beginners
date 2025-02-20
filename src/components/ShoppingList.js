7d3daad66a75df831f79d200e5a1779ce66e9597
antobnl <bannielantoine@gmail.com>
2025-02-20 12:13:45 +0100

    commit pour exo partie 2

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,7 +1,18 @@
 import { plantList } from '../datas/plantList'
 
 function ShoppingList() {
-	return <ul></ul>
+	let categories = []
+	plantList.forEach(plant => {
+		if(!categories.includes(plant.category)){
+			categories.push(plant.category)
+		}		
+	});
+	return (<ul>
+		{categories.map((category, index) => (
+			<li key={index}> {category} </li>
+		))}
+	</ul>)
 }
+
 
 export default ShoppingList
